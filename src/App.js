8180ede0380a4da8ed7acd5519467e05d2d64fb3
Tim3Ds <Tim3Ds@gmail.com>
2017-03-22 20:29:46 -0600

    table :)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import "../node_modules/react-vis/dist/style.css";
+
+import {XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries} from 'react-vis';
 
 class App extends Component {
   render() {
@@ -13,6 +17,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <XYPlot
+          width={300}
+          height={300}>
+          <HorizontalGridLines />
+          <LineSeries
+            data={[
+              {x: 1, y: 10},
+              {x: 2, y: 5},
+              {x: 3, y: 15}
+            ]}/>
+          <XAxis />
+          <YAxis />
+        </XYPlot>
       </div>
     );
   }
